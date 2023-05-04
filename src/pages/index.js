03a693a79938fc6e9cdfb27f65c49d22d700b147
TheFred94 <frederikrlarsen@gmail.com>
2023-05-04 08:58:11 +0200

    removed value from option

--- src/pages/index.js
+++ src/pages/index.js
@@ -122,9 +122,7 @@ export function SellerEstateForm() {
       <label>
         <span className={styles.label}>Property type</span>
         <select className="inputs" id="selectInput" name="propertyType">
-          <option onChange={handleSelectChange} value="">
-            Please choose...
-          </option>
+          <option onChange={handleSelectChange}>Please choose...</option>
           <EstOption />
         </select>
       </label>
